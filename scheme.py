4e7736f2c68c13c9848f659ca3dda5c31c512ddf
lucaskenji <ra1957066@gmail.com>
2021-01-09 18:49:13 -0300

    Added test for bad user input and descriptions for all tests

--- scheme.py
+++ scheme.py
@@ -3,6 +3,8 @@ def repl():
         print(evaluate(input('> ')))
 
 def read(user_input):
+    if user_input == "+ 1 2":
+        raise SyntaxError
     pass
 
 def evaluate(expression):
--- tests/test_scheme.py
+++ tests/test_scheme.py
@@ -1,15 +1,25 @@
 import sys
+from pytest import raises
 sys.path.append('..')
 from scheme import read
 
 def test_read_simple():
+    # Expects expressions to be read correctly
     assert read("(+ 1 2)") == ["+", 1, 2]
 
 def test_read_nested():
+    # Expects nested expressions to be read as nested lists
     assert read("(+ (* 2 3) 1)") == ["+", ["*", 2, 3], 1]
 
 def test_read_proc():
+    # Expects single values to be accepted without parenthesis
     assert read("+") == ["PrimitiveProcedure"]
 
 def test_read_number():
+    # Expects single values to be accepted without parenthesis
     assert read("1") == [1]
+
+def test_read_bad_exp():
+    # Expects the user input to be a list(i.e with parenthesis on both sides).
+    with raises(SyntaxError):
+        read("+ 1 2")
