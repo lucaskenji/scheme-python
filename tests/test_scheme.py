d89d94f6c0cda8e3e9641aac40ccc49719c32dea
lucaskenji <ra1957066@gmail.com>
2021-01-09 20:43:24 -0300

    Implemented read function. Also slightly changed test specifications to expect strings

--- tests/test_scheme.py
+++ tests/test_scheme.py
@@ -5,19 +5,19 @@ from scheme import read
 
 def test_read_simple():
     # Expects expressions to be read correctly
-    assert read("(+ 1 2)") == ["+", 1, 2]
+    assert read("(+ 1 2)") == ["+", "1", "2"]
 
 def test_read_nested():
     # Expects nested expressions to be read as nested lists
-    assert read("(+ (* 2 3) 1)") == ["+", ["*", 2, 3], 1]
+    assert read("(+ (* 2 3) 1)") == ["+", ["*", "2", "3"], "1"]
 
 def test_read_proc():
     # Expects single values to be accepted without parenthesis
-    assert read("+") == ["PrimitiveProcedure"]
+    assert read("+") == ["+"]
 
 def test_read_number():
     # Expects single values to be accepted without parenthesis
-    assert read("1") == [1]
+    assert read("1") == ["1"]
 
 def test_read_bad_exp():
     # Expects the user input to be a list(i.e with parenthesis on both sides).
