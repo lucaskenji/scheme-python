53c5906632f4319af8f9633438e566ceb928a053
lucaskenji <ra1957066@gmail.com>
2021-01-11 20:07:40 -0300

    Added tests for addition function

---
+++ tests/test_primitives.py
@@ -0,0 +1,13 @@
+import sys
+from pytest import raises
+sys.path.append('..')
+from primitives import primitives
+
+def test_primitive_add:
+    assert primitives["+"]("1") == 1
+    assert primitives["+"]("1", "2") == 3
+    assert primitives["+"]("1", "2", "3") == 6
+
+def test_primitive_bad_add:
+    with raises(TypeError):
+        primitives["+"]("2", "+")
