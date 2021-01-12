779e3a0b4e144025b6eadf68a4c2968a0c064afb
lucaskenji <ra1957066@gmail.com>
2021-01-12 19:14:59 -0300

    Implemented multiplication primitive

--- primitives.py
+++ primitives.py
@@ -9,6 +9,16 @@ def add(*args):
         raise TypeError("All arguments to '+' must be numbers.")
     return str(reduce(lambda a, b: a + b, operands))
 
+def multiply(*args):
+    operands = []
+    try:
+        for operand in args:
+            operands.append(int(operand))
+    except ValueError:
+        raise TypeError("All arguments to '*' must be numbers.")
+    return str(reduce(lambda a, b: a * b, operands))
+
 primitives = {
-    "+": add
+    "+": add,
+    "*": multiply
 }
