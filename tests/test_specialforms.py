6fc96a92a67ec4758eec00c423d6915cd9c34e33
lucaskenji <ra1957066@gmail.com>
2021-01-26 19:51:41 -0300

    Added tests for special forms

---
+++ tests/test_specialforms.py
@@ -0,0 +1,21 @@
+import sys
+from pytest import raises
+sys.path.append('..')
+from special_forms import special_forms
+
+def test_define_bad_args():
+    # Expects an error if define is called with wrong number of arguments
+    with raises(TypeError):
+        special_forms["define"]("a", "b", "c", "d")
+
+def test_define_new_var():
+    # Expects a new variable to be defined if it doesn't exist
+    test_env = ({"a": "1"}, None)
+    special_forms["define"](test_env, "b", "3")
+    assert test_env[0]["b"] == "3"
+
+def test_define_assign():
+    # Expects a new value to be assigned into a previously defined variable
+    test_env ({"a": "1"}, None)
+    special_forms["define"](test_env, "a", "3")
+    assert test_env[0]["a"] == "3"
